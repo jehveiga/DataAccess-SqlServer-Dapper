a565d09707a75a222813e6a5320dc3e99efeea91
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-09-14 11:52:38 -0300

    Executar a primeira consulta usando Dapper

---
+++ SqlServer_Dapper/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace SqlServer_Dapper.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
--- SqlServer_Dapper/Program.cs
+++ SqlServer_Dapper/Program.cs
@@ -1,6 +1,19 @@
-﻿// Pacotes instalados
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SqlServer_Dapper.Models;
+
+// Pacotes instalados
 // Microsoft.Data.SqlClient
 // Dapper
 
-const string connectionString = "Server=localhost,1433;Database=balta;Integrated Security=SSPI";
+const string connectionString = "Server=localhost;Database=balta;Integrated Security=true;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
 
+using (var connection = new SqlConnection(connectionString))
+{
+    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+    foreach (var category in categories)
+    {
+        Console.WriteLine($"{category.Id} - {category.Title}");
+    }
+}
